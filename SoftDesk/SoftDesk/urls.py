09feb66bd6f28ba7020b9a396512b1fa7d73bfd3
Rimonteil <nardiotencha@gmail.com>
2022-05-05 11:50:32 +0200

    user creation per api

--- SoftDesk/SoftDesk/urls.py
+++ SoftDesk/SoftDesk/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from users.views import CreateUserAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('signup/', CreateUserAPIView.as_view())
+
 ]
