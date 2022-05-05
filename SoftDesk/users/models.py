aca1f7407c1d5dc5620851cf802c5b6fb3ebab6d
Rimonteil <nardiotencha@gmail.com>
2022-05-05 10:44:48 +0200

    model user and usermanager

--- SoftDesk/users/models.py
+++ SoftDesk/users/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+from django.db import models, transaction
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError('Un email est necessaire')
+        
+        try:
+            with transaction.atomic():
+                user = self.model(email=email, **extra_fields)
+                user.set_password(password)
+                user.save(using=self._db)
+                return user
+        except:
+            raise
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, extra_fields)
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+
+    email = models.EmailField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        return self
