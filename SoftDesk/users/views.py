3f870b0be36a91ba002504b66b75b3dd37a344f1
Rimonteil <nardiotencha@gmail.com>
2022-05-05 12:07:30 +0200

    correct import

--- SoftDesk/users/views.py
+++ SoftDesk/users/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 
-from serializers import UserSerializer
+from users.serializers import UserSerializer
 
 
 class CreateUserAPIView(APIView):
