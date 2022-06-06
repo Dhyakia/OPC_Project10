5e4d7fc75ea3f4ee0a91e6ef7eca1b963f338402
Rimonteil <nardiotencha@gmail.com>
2022-06-06 15:24:13 +0200

    added infos to routers

--- SoftDesk/SoftDesk/urls.py
+++ SoftDesk/SoftDesk/urls.py
@@ -7,16 +7,23 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users import views as userV
 from projects import views as projectsV
 
-
+# projects/
+# projects/<pk>/
 router = routers.SimpleRouter()
 router.register(r'projects', projectsV.ProjectsViewset)
 
+# projects/<projects_pk>/users/
+# projects/<projects_pk>/users/<pk>/
 projects_user_router = routers.NestedSimpleRouter(router, r'projects', lookup='projects')
 projects_user_router.register(r'users', projectsV.ContributorsViewset)
 
+# projects/<projects_pk>/issues/
+# projects/<projects_pk>/issues/<pk>/
 projects_issue_router = routers.NestedSimpleRouter(router, r'projects', lookup='projects')
 projects_issue_router.register(r'issues', projectsV.IssuesViewset)
 
+# projects/<projects_pk>/issues/<issues_pk>/comments/
+# projects/<projects_pk>/issues/<issues_pk>/comments/<pk>/
 projects_issue_comment_router = routers.NestedSimpleRouter(projects_issue_router, r'issues', lookup='issues')
 projects_issue_comment_router.register(r'comments', projectsV.CommentsViewset)
 
